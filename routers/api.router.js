3b7ec14787b9b0adbdc9cf3aa44803df05a79411
«julieSalha» <julie.salha@gmail.com>
2020-04-09 19:04:30 +0200

    update routes connection mysql npm

--- routers/api.router.js
+++ routers/api.router.js
@@ -23,9 +23,36 @@ Routes definition
             /* 
             CRUD: Create route 
             */
-                router.post('/:endpoint', (req, res) => {
-                    return res.json({ msg: 'CRUD: Create route', endpoint: req.params.endpoint })
-                })
+           router.post('/:endpoint', (req, res) => {
+            // Set MySQL connection
+            const connection = mysql.createConnection({
+                host     : 'localhost',
+                port     :  8889,
+                user     : 'root',
+                password : 'root',
+                database : 'api_node'
+            })
+            
+            // Connect the DB
+            connection.connect( (connectionError) => {
+                if (connectionError) {
+                    return res.json({ msg: 'MYSQL: error connecting', err: connectionError })
+                }
+                else{
+                    connection.query('INSERT INTO post SET ?', {
+                        title: req.body.title,
+                        content: req.body.content
+                    }, (queryError, results, fields) => {
+                        if (queryError) {
+                            return res.json({ msg: 'MYSQL: error query', err: queryError })
+                        }
+                        else{
+                            return res.json({ msg: 'MYSQL: OK query', results: results, fields: fields })
+                        }
+                    });
+                };
+            });
+        })
             //
 
             /* 
@@ -40,45 +67,132 @@ Routes definition
                 database : 'api_node'
             })
             connection.connect( (connectionError) => {
-                if (connectionError) {
-                    return res.json({ msg: 'MYSQL: error connecting', err: connectionError })
-                }
-                else{
-                    // Get all item from table :endpoint
-                    connection.query(`SELECT * FROM ${req.params.endpoint}`, (queryError, results, fields) => {
-                        if (queryError) {
-                            return res.json({ msg: 'MYSQL: error query', err: queryError })
-                        }
-                        else{
-                            return res.json({ msg: 'MYSQL: OK query', results: results, fields: fields })
-                        }
-                    });
-                };
-            });
-        })
+                    if (connectionError) {
+                        return res.json({ msg: 'MYSQL: error connecting', err: connectionError })
+                    }
+                    else{
+                        // Get all item from table :endpoint
+                        connection.query(`SELECT * FROM ${req.params.endpoint}`, (queryError, results, fields) => {
+                            if (queryError) {
+                                return res.json({ msg: 'MYSQL: error query', err: queryError })
+                            }
+                            else{
+                                return res.json({ msg: 'MYSQL: OK query', results: results, fields: fields })
+                            }
+                        });
+                    };
+                });
+            })
             //
 
             /* 
             CRUD: Read one route
             */
+                // Afficher un post
+
                 router.get('/:endpoint/:id', (req, res) => {
-                    return res.json({ msg: 'CRUD: Read one route', endpoint: req.params.endpoint, id: req.params.id })
-                })
+                    const connection = mysql.createConnection({
+                        host     : 'localhost',
+                        port     :  8889,
+                        user     : 'root',
+                        password : 'root',
+                        database : 'api_node'
+                    })
+                    connection.connect( (connectionError) => {
+                            if (connectionError) {
+                                return res.json({ msg: 'MYSQL: error connecting', err: connectionError })
+                            }
+                            else{
+                                // Get one post with an id
+                                connection.query(`SELECT * FROM ${req.params.endpoint} WHERE id = ${req.params.id}`, (queryError, results, fields) => {
+                                    if (queryError) {
+                                        return res.json({ msg: 'MYSQL: error query', err: queryError })
+                                    }
+                                    else{
+                                        return res.json({ msg: 'MYSQL: OK query', results: results, fields: fields })
+                                    }
+                                });
+                            };
+                        });
+                    })
             //
 
             /* 
             CRUD: Update route 
             */
+                // router.put('/:endpoint/:id', (req, res) => {
+                //     return res.json({ msg: 'CRUD: Update route', endpoint: req.params.endpoint, id: req.params.id })
+                // })
                 router.put('/:endpoint/:id', (req, res) => {
-                    return res.json({ msg: 'CRUD: Update route', endpoint: req.params.endpoint, id: req.params.id })
+                    // Set MySQL connection
+                    const connection = mysql.createConnection({
+                        host     : 'localhost',
+                        port     :  8889,
+                        user     : 'root',
+                        password : 'root',
+                        database : 'api_node'
+                    })
+                    
+                    // Connect the DB
+                    connection.connect( (connectionError) => {
+                        if (connectionError) {
+                            return res.json({ msg: 'MYSQL: error connecting', err: connectionError })
+                        }
+                        else{
+                            // Get all item from table :endpoint
+                            connection.query(`
+                                UPDATE ${req.params.endpoint} 
+                                SET title = "${req.body.title}", content = "${req.body.content}" 
+                                WHERE id = ${req.params.id}
+                            `, (queryError, results, fields) => {
+                                if (queryError) {
+                                    return res.json({ msg: 'MYSQL: error query', err: queryError })
+                                }
+                                else{
+                                    return res.json({ msg: 'MYSQL: OK query', results: results, fields: fields })
+                                }
+                            });
+                        };
+                    });
                 })
             //
 
             /* 
             CRUD: Delete route 
             */
+                // router.delete('/:endpoint/:id', (req, res) => {
+                //     return res.json({ msg: 'CRUD: Delete route', endpoint: req.params.endpoint, id: req.params.id })
+                // })
+
                 router.delete('/:endpoint/:id', (req, res) => {
-                    return res.json({ msg: 'CRUD: Delete route', endpoint: req.params.endpoint, id: req.params.id })
+                    // Set MySQL connection
+                    const connection = mysql.createConnection({
+                        host     : 'localhost',
+                        port     :  8889,
+                        user     : 'root',
+                        password : 'root',
+                        database : 'api_node'
+                    })
+                    
+                    // Connect the DB
+                    connection.connect( (connectionError) => {
+                        if (connectionError) {
+                            return res.json({ msg: 'MYSQL: error connecting', err: connectionError })
+                        }
+                        else{
+                            connection.query(`
+                                DELETE FROM ${req.params.endpoint}  
+                                WHERE id = ${req.params.id}
+                            `, (queryError, results, fields) => {
+                                if (queryError) {
+                                    return res.json({ msg: 'MYSQL: error query', err: queryError })
+                                }
+                                else{
+                                    return res.json({ msg: 'MYSQL: OK query', results: results, fields: fields })
+                                }
+                            });
+                        };
+                    });
                 })
             //            
         };
